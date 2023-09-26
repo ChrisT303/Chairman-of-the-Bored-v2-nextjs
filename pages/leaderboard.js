8cc93a0fbf04fb2198971c71988bc39ff96f8ca6
deftonechris <deftonechris@msn.com>
2023-09-26 13:35:37 -0600

    leaderboard UI complete

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -5,31 +5,31 @@ import { GET_TOP_USERS } from "../server/utils/queries";
 
 const Leaderboard = () => {
   const { data: leaderboardData } = useQuery(GET_TOP_USERS);
-  console.log(leaderboardData);
-
+  
   return (
-    <div className="flex flex-col bg-leaderboard bg-cover bg-center h-screen p-8">
-      <div className="flex flex-col space-y-4 bg-yellow-300 p-8 rounded-xl">
-        <table className="table-fixed w-6/12">
+    <div className="flex items-center justify-center bg-leaderboard bg-cover bg-center h-screen p-8">
+      <div className="flex flex-col items-center justify-center bg-yellow-300 space-y-4 p-8 rounded-xl max-w-screen-lg mx-auto w-full">
+        <table className="table-fixed w-6/12 mb-8">
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Score</th>
+              <th className="sm:text-5xl text-4xl">Name</th>
+              <th className="sm:text-5xl text-4xl">Score</th>
             </tr>
           </thead>
-          <tbody>
-  {leaderboardData?.getTopUsers?.map((user) => (
-    <tr key={user.id}>
-      <td>{user.username}</td>
-      <td>{user.points}</td>
-    </tr>
-  ))}
-</tbody>
-
+          <tbody >
+            {leaderboardData?.getTopUsers?.map((user) => (
+              <tr key={user.id}>
+                <td className="text-center text-2xl">{user.username}</td>
+                <td className="text-center text-2xl">{user.points}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
   );
 };
+
+
 
 export default Leaderboard;
