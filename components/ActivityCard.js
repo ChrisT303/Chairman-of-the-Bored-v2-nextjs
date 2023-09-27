2cc485d847506f58369faf2d6fd2572ad4c8f727
deftonechris <deftonechris@msn.com>
2023-09-27 12:22:54 -0600

    UI changing for completed activity on card, moving to reflect on backend

--- components/ActivityCard.js
+++ components/ActivityCard.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { formatPrice } from '../server/utils/priceFormatter'; 
 
 
 const ActivityCard = ({ savedActivity, onDelete, onCompletion }) => {
+  const [isCompleted, setIsCompleted] = useState(false); 
+  
+  const handleCompletion = () => {
+    setIsCompleted(true); 
+    onCompletion(savedActivity.id);
+  };
+
   return (
-    <div className="retro-border rounded-lg overflow-hidden shadow-lg m-2 p-4 bg-retro-background flex justify-between items-start">
-      <div>
-        {/* <h3 className="font-bold text-xl mb-2 retro-text">{savedActivity.activity.activity}</h3> */}
-        <h3 className="font-bold text-xl mb-2 retro-text">
-          {savedActivity.activity.type}
-        </h3>
+    <div 
+    className={`retro-border rounded-lg overflow-hidden shadow-lg m-2 p-4 
+    ${isCompleted ? 'bg-green-200' : 'bg-retro-background'} flex justify-between items-start`}>
+    <div>
+      <h3 className="font-bold text-xl mb-2 retro-text">
+        {savedActivity.activity.type}
+      </h3>
+
         <p className="retro-text">
           Participants: {savedActivity.activity.participants}
         </p>
         <p className="retro-text">Price: {formatPrice(savedActivity.activity.price)}</p>
       </div>
       <div className="flex flex-col space-y-2">
-        <button
-          className="retro-btn bg-green-500 px-4 py-2"
-          onClick={() => onCompletion(savedActivity.id)}
-          >
+      <button
+          className={`retro-btn ${isCompleted ? 'bg-gray-400' : 'bg-green-500'} px-4 py-2`}
+          onClick={handleCompletion}
+          disabled={isCompleted}
+        >
           Complete
         </button>
         <button
--- server/utils/mutations.js
+++ server/utils/mutations.js
@@ -82,3 +82,7 @@ export const INCREMENT_USER_POINTS = gql`
 
 
 
+
+
+
+
